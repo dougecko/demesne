b1a5c26ff30e1129ac2d2010f91b6ed979426b35
Doug Hagan <douglas.hagan@gmail.com>
2025-06-10 15:25:10 +1000

    Remove commented-out code and unnecessary elements from App component for cleaner structure and improved readability.

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -15,44 +15,13 @@ const App: FC = () => {
         <div className="app">
             <header className="app-header">
                 <img src={demesneLogo} className="app-logo" alt="Demesne Logo" />
-                <h1>Demesne</h1>
-{/*
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-*/}
             </header>
-
-{/*
-            <h1>Demesne</h1>
-
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-*/}
-
             <main>
                 <div className="lists-container">
                     <SpellList />
                     <CreatureList />
                 </div>
             </main>
-
-{/*
-            <ExampleComponent />
-
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-*/}
         </div>
     )
 }
